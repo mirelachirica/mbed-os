a9361f6894f5ee115554772c13683dc624c8522f
Jari Poyhonen <Jari.Poyhonen@arm.com>
2018-10-24 10:55:23 +0300

    add TC user_rx_read_1_byte_per_read_call_max_size_user_payload_available

--- UNITTESTS/features/cellular/framework/mux/muxtest.cpp
+++ UNITTESTS/features/cellular/framework/mux/muxtest.cpp
@@ -4094,3 +4094,101 @@ TEST_F(TestMux, user_rx_read_max_size_user_payload_in_1_read_call)
     /* Validate proper callback callcount. */
     EXPECT_EQ(1, m_user_rx_read_max_size_user_payload_in_1_read_call_check_value);
 }
+
+
+static uint8_t m_user_rx_read_1_byte_per_read_call_max_size_user_payload_available_check_value = 0;
+static void rx_read_1_byte_per_read_call_max_size_user_payload_available_callback()
+{
+    ++m_user_rx_read_1_byte_per_read_call_max_size_user_payload_available_check_value;
+}
+
+
+/*
+ * TC - Ensure that Rx frame read works correctly when max amount of user data is received and read is done 1 byte a
+ * time.
+ *
+ * Test sequence:
+ * - Establish 1 DLCI
+ * - Generate user RX data frame
+ * - Verify read buffer upon frame read complete
+ *
+ * Expected outcome:
+ * - Read buffer verified
+ * - Correct callback count
+ */
+TEST_F(TestMux, user_rx_read_1_byte_per_read_call_max_size_user_payload_available)
+{
+    m_user_rx_read_1_byte_per_read_call_max_size_user_payload_available_check_value = 0;
+
+    InSequence dummy;
+
+    mbed::Mux3GPP obj;
+
+    events::EventQueue eq;
+    obj.eventqueue_attach(&eq);
+
+    MockFileHandle fh_mock;
+    SigIo          sig_io;
+    EXPECT_CALL(fh_mock, sigio(_)).Times(1).WillOnce(Invoke(&sig_io, &SigIo::sigio));
+    EXPECT_CALL(fh_mock, set_blocking(false)).WillOnce(Return(0));
+
+    obj.serial_attach(&fh_mock);
+
+    MuxCallbackTest callback;
+    obj.callback_attach(mbed::Callback<void(mbed::MuxBase::event_context_t &)>(&callback,
+                        &MuxCallbackTest::channel_open_run), mbed::MuxBase::CHANNEL_TYPE_AT);
+
+    /* Establish a user channel. */
+
+    mux_self_iniated_open(callback, FRAME_TYPE_UA, obj, fh_mock, sig_io);
+
+    /* Validate Filehandle generation. */
+    EXPECT_TRUE(callback.is_callback_called());
+    m_file_handle[0] = callback.file_handle_get();
+    EXPECT_TRUE(m_file_handle[0] != NULL);
+
+    m_file_handle[0]->sigio(rx_read_1_byte_per_read_call_max_size_user_payload_available_callback);
+
+    /* Program read cycle. */
+    uint8_t read_byte[RX_BUFFER_SIZE - 1u] = {0};
+    read_byte[0]                           = 1u | (DLCI_ID_LOWER_BOUND << 2);
+    read_byte[1]                           = FRAME_TYPE_UIH;
+    read_byte[2]                           = LENGTH_INDICATOR_OCTET | ((sizeof(read_byte) - 5u) << 1);
+
+    sequence_generate(&(read_byte[3]), (sizeof(read_byte) - 5u));
+
+    read_byte[sizeof(read_byte) - 2] = fcs_calculate(&read_byte[0], 3u);
+    read_byte[sizeof(read_byte) - 1] = FLAG_SEQUENCE_OCTET;
+
+    single_complete_read_cycle(&(read_byte[0]),
+                               sizeof(read_byte),
+                               SUSPEND_RX_CYCLE,
+                               NULL,
+                               0,
+                               ENQUEUE_DEFERRED_CALL_YES,
+                               fh_mock,
+                               sig_io);
+
+    /* Verify read buffer: do reads 1 byte a time until all of the data is read. */
+    ssize_t read_ret;
+    uint8_t test_buffer[sizeof(read_byte) - 5u] = {0};
+    uint8_t read_count                          = 0;
+    do {
+        if ((read_count + 1u) == sizeof(test_buffer)) {
+            mbed_equeue_stub::call_expect(1);
+        }
+
+        read_ret = m_file_handle[0]->read(&(test_buffer[read_count]), 1u);
+        EXPECT_EQ(1, read_ret);
+        EXPECT_EQ(read_byte[3u + read_count], test_buffer[read_count]);
+
+        ++read_count;
+    } while (read_count != sizeof(test_buffer));
+
+    /* Verify read buffer empty. */
+    read_ret = m_file_handle[0]->read(&(test_buffer[0]), 1u);
+    EXPECT_EQ(-EAGAIN, read_ret);
+
+    /* Validate proper callback callcount. */
+    EXPECT_EQ(1, m_user_rx_read_1_byte_per_read_call_max_size_user_payload_available_check_value);
+}

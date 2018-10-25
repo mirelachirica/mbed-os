c5d77f5798435994893cb33d22a5be1471e548c2
Jari Poyhonen <Jari.Poyhonen@arm.com>
2018-10-25 10:09:54 +0300

    add TC rx_frame_type_not_supported

--- UNITTESTS/features/cellular/framework/mux/muxtest.cpp
+++ UNITTESTS/features/cellular/framework/mux/muxtest.cpp
@@ -4613,3 +4613,113 @@ TEST_F(TestMux, user_rx_invalid_fcs)
     /* Validate proper callback callcount. */
     EXPECT_EQ(1, m_user_rx_invalid_fcs_check_value);
 }
+
+
+static uint8_t m_rx_frame_type_not_supported_check_value = 0;
+static void rx_frame_type_not_supported_callback()
+{
+    ++m_rx_frame_type_not_supported_check_value;
+}
+
+
+/*
+ * TC - Ensure proper behaviour when Rx frame received with invalid frame type ID
+ *
+ * Test sequence:
+ * - Mux3GPP open
+ * - Establish a DLCI
+ * - Rx frame with invalid frame type ID: silently discarded by the implementation
+ * - Rx frame with valid frame type ID: accepted by the implementation.
+ *
+ * Expected outcome:
+ * - The invalid frame type ID Rx frame is dropped by the implementation
+ * - The valid frame type ID Rx frame is accepted by the implementation
+ * - Validate proper callback callcount
+ * - Validate read buffer
+ */
+TEST_F(TestMux, rx_frame_type_not_supported)
+{
+    m_rx_frame_type_not_supported_check_value = 0;
+
+    InSequence dummy;
+
+    mbed::Mux3GPP obj;
+
+    events::EventQueue eq;
+    obj.eventqueue_attach(&eq);
+
+    MockFileHandle fh_mock;
+    SigIo          sig_io;
+    EXPECT_CALL(fh_mock, sigio(_)).Times(1).WillOnce(Invoke(&sig_io, &SigIo::sigio));
+    EXPECT_CALL(fh_mock, set_blocking(false)).WillOnce(Return(0));
+
+    obj.serial_attach(&fh_mock);
+
+    MuxCallbackTest callback;
+    obj.callback_attach(mbed::Callback<void(mbed::MuxBase::event_context_t &)>(&callback,
+                        &MuxCallbackTest::channel_open_run), mbed::MuxBase::CHANNEL_TYPE_AT);
+
+    /* Establish a user channel. */
+
+    mux_self_iniated_open(callback, FRAME_TYPE_UA, obj, fh_mock, sig_io);
+
+    /* Validate Filehandle generation. */
+    EXPECT_TRUE(callback.is_callback_called());
+    m_file_handle[0] = callback.file_handle_get();
+    EXPECT_TRUE(m_file_handle[0] != NULL);
+
+    m_file_handle[0]->sigio(rx_frame_type_not_supported_callback);
+
+    /* Establish a DLCI. */
+
+    /* Rx frame with invalid frame type ID: silently discarded by the implementation. */
+    const uint8_t user_data = 0xA5u;
+    const uint8_t dlci_id   = DLCI_ID_LOWER_BOUND;
+    uint8_t read_byte[6]    =
+    {
+        1u | (dlci_id << 2),
+        FRAME_TYPE_UNSUPPORTED,
+        LENGTH_INDICATOR_OCTET | (sizeof(user_data) << 1),
+        user_data,
+        fcs_calculate(&read_byte[0], 3u),
+        FLAG_SEQUENCE_OCTET
+    };
+    single_complete_read_cycle(&(read_byte[0]),
+                               sizeof(read_byte),
+                               RESUME_RX_CYCLE,
+                               NULL,
+                               0,
+                               ENQUEUE_DEFERRED_CALL_YES,
+                               fh_mock,
+                               sig_io);
+
+    /* Validate proper callback callcount. */
+    EXPECT_EQ(0, m_rx_frame_type_not_supported_check_value);
+
+    /* Rx frame with valid frame type ID: accepted by the implementation. */
+    read_byte[1] = FRAME_TYPE_UIH;
+    read_byte[4] = fcs_calculate(&read_byte[0], 3u);
+    single_complete_read_cycle(&(read_byte[0]),
+                               sizeof(read_byte),
+                               SUSPEND_RX_CYCLE,
+                               NULL,
+                               0,
+                               ENQUEUE_DEFERRED_CALL_YES,
+                               fh_mock,
+                               sig_io);
+
+    /* Validate proper callback callcount. */
+    EXPECT_EQ(1, m_rx_frame_type_not_supported_check_value);
+
+    /* Validate read buffer. */
+    mbed_equeue_stub::call_expect(1);
+    uint8_t test_buffer[1] = {0};
+    ssize_t read_ret       = m_file_handle[0]->read(&(test_buffer[0]), 1u);
+    EXPECT_EQ(1, read_ret);
+    EXPECT_EQ(user_data, test_buffer[0]);
+    read_ret = m_file_handle[0]->read(&(test_buffer[0]), 1u);
+    EXPECT_EQ(-EAGAIN, read_ret);
+
+    /* Validate proper callback callcount. */
+    EXPECT_EQ(1, m_rx_frame_type_not_supported_check_value);
+}
